3892ba75540f32d9f803e2b32c9b7acea0565e4c
Alex Jonsson <153451620+CKCHDX@users.noreply.github.com>
2026-03-16 08:40:24 +0100

    Delete SLP directory

--- SLP/csh/services/Klar - Server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting C:\Users\akoj2\Desktop\Sverkan\sverkan\RUNME.bat...
-call "C:\Users\akoj2\Desktop\Sverkan\RUNME.bat"
-echo RUNME.bat finished.
--- SLP/csh/services/Upsum - Serve.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting C:\Users\akoj2\Desktop\upsum\RUNME.bat...
-call "C:\Users\akoj2\Desktop\upsum\RUNME.bat"
-echo RUNME.bat finished.
